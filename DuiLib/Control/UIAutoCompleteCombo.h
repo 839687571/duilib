1f67ccf1cca01f02fb49c25a0f27c2b9b6369a33
839687571 <839687571@qq.com>
2015-08-26 09:48:37 +0800

    Tab选择的时候, combo控件 不要获取焦点.由内部成员Editui 处理.

--- DuiLib/Control/UIAutoCompleteCombo.h
+++ DuiLib/Control/UIAutoCompleteCombo.h
@@ -24,14 +24,19 @@ public:
 	bool OnComboNotify(void* pMsg);
 	bool OnInitControl(void* pMsg);
 
+
 	void SetPos(RECT rc,  bool bNeedInvalidate);
 
 	CDuiString GetEditText();
+
+
 
 	/* 重新的combui 方法*/
 	/*********************************************************/
 	void SetEnabled(bool bEnable = true);
 	virtual const CDuiString& GetUserData(); /* selected item userdata */
+	virtual UINT GetControlFlags() const;
+	virtual void SetFocus();
 	/*********************************************************/
 private:
 	void SetText(int iComboSel);
